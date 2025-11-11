108760d98ee114205e0515f29c69682c50f4d0ac
Het Parekh <hetparekh09@gmail.com>
2025-11-12 01:12:49 +0530

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {createServer} from "node:http";
+
+import {Server} from "socket.io";
+
+import mongoose from "mongoose";
+
+import cors from "cors";
+
+const port = 8000;
+
+ const app = express();
+
+ app.get("/home"  , (req,res)=>{
+    return res.json({"hello" : "world"})
+ }); 
+
+ const start = async ()=>{
+    
+    app.listen(port, ()=>{
+        console.log("listening on port 8000");
+    })
+ }
+
+
+ start();
