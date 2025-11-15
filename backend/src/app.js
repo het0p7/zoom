0fadb68b4fcf313939c37a176294454107bf5adc
Het Parekh <hetparekh09@gmail.com>
2025-11-15 21:29:28 +0530

    create register, login routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,37 +1,42 @@
 import express from "express";
-import {createServer} from "node:http";
+import { createServer } from "node:http";
 import dotenv from "dotenv";
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 
 import mongoose from "mongoose";
 
 import cors from "cors";
-
+import userRoutes from "./routes/users.routes.js";
 
 const uri = process.env.MONGO_URL;
 
 const port = 8000;
 
- const app = express();
- const server = createServer(app);
- const io = new Server(server); 
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
- app.set("port",(process.env.PORT || 8000))
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
+app.use("/api/v1/users", userRoutes);
 
- app.get("/home"  , (req,res)=>{
-    return res.json({"hello" : "world"})
- }); 
+app.get("/home", (req, res) => {
+  return res.json({ hello: "world" });
+});
 
- const start = async ()=>{
- app.set("mongo_user")
-   const connectionDB = await mongoose.connect("mongodb+srv://hetparekh09_db_user:zerodhaclonehet@zoom.dee1isl.mongodb.net/");
-   console.log(`MONGO connected DB Host = ${connectionDB.connection.host}`);
+const start = async () => {
+  app.set("mongo_user");
+  const connectionDB = await mongoose.connect(
+    "mongodb+srv://hetparekh09_db_user:zerodhaclonehet@zoom.dee1isl.mongodb.net/"
+  );
+  console.log(`MONGO connected DB Host = ${connectionDB.connection.host}`);
 
-    server.listen(app.get("port"), ()=>{
-        console.log("listening on port 8000");
-    })
- }
+  server.listen(app.get("port"), () => {
+    console.log("listening on port 8000");
+  });
+};
 
-
- start();
+start();
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,32 @@
 import httpStatus from "http-status";
-import { User } from "../models/user.model";
+import { User } from "../models/user.model.js";
 import bcrypt, { hash } from "bcrypt";
+import crypto from "crypto";
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Please Provide" });
+  }
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: "User Not Found" });
+    }
+    if (bcrypt.compare(password, user.password)) {
+      let token = crypto.randomBytes(20).toString("hex");
+      user.token = token;
+      await user.save();
+      return res.status(httpStatus.OK).json({ token: token });
+    }
+  } catch (e) {
+    return res.status(500).json({ message: `SOmething went wwrong ${e}` });
+  }
+};
 
 const register = async (req, res) => {
   const { name, username, password } = req.body;
@@ -18,12 +44,14 @@ const register = async (req, res) => {
     const newUser = new User({
       name: name,
       username: username,
-      password: hashedPassword, 
+      password: hashedPassword,
     });
     await newUser.save();
 
-    res.status(httpStatus.CREATED).json({message:"User Registered"})
-  } catch(e){
-    res.json({message:`Something went worng${e}`})
+    res.status(httpStatus.CREATED).json({ message: "User Registered" });
+  } catch (e) {
+    res.json({ message: `Something went worng${e}` });
   }
 };
+
+export { login, register };
